e6c31b1a4b58661d62dd18efd75c585e5b7ba278
AS <agungsaepudin@team.youapp.ai>
2024-04-25 20:16:35 +0700

    added cors middleware

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from 'cors';
+
 import { router } from "../routes/index.route";
 import { errorMiddleware } from "../middlewares/index.middleware";
 
 export const app = express()
 app.use(express.json())
+app.use(cors({ origin: "*" }))
 app.use(router)
 app.use(errorMiddleware)
